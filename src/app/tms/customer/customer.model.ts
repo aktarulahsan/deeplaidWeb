711f0c1669a9ac8f224e2b8141450e099255f78d
aktarulahsan <md.aktarulahsan@gmail.com>
2021-02-01 17:07:28 +0600

    change customer info

--- src/app/tms/customer/customer.model.ts
+++ src/app/tms/customer/customer.model.ts
@@ -5,6 +5,8 @@ import { BaseModel } from "../model/base.Model";
 export class Customer extends BaseModel{
 
     cusId:Number;
+    companyId:string;
+    branchId:string;
     customerName:string;
     gender:string;
     mobile:string;
